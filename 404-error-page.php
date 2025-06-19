bdf51797b5597773cd2bcb5e6b824f3b526d7e50
Joe Foray <89590401+jforay@users.noreply.github.com>
2025-06-19 13:30:13 -0400

    Add files via upload
    first 100, excluding folders

---
+++ 404-error-page.php
@@ -0,0 +1,14 @@
+<div class="mars"></div>
+<div align="center">
+<img src="https://assets.codepen.io/1538474/404.svg" class="logo-404" />
+<img src="https://assets.codepen.io/1538474/meteor.svg" class="meteor" />
+<p class="title">Oh no!!</p>
+<p class="subtitle">
+	You’re either misspelling the URL <br /> or requesting a page that's no longer here.
+</p>
+
+	<a class="btn-back" href="https://destinationboatclubscarolinas.com/">Back to previous page</a>
+
+<img src="https://assets.codepen.io/1538474/astronaut.svg" class="astronaut" />
+<img src="https://assets.codepen.io/1538474/spaceship.svg" class="spaceship" />
+</div>
